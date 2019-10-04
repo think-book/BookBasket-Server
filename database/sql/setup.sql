0523161c41fdc87a0cd564d3c7b1a8c40bb3e69b
noborack74 <46844526noborack74@users.noreply.github.com>
2019-10-04 16:18:37 +0900

    初期DB設定 (WIP)

--- database/sql/setup.sql
+++ database/sql/setup.sql
@@ -20,18 +20,18 @@ INSERT INTO bookbasket.bookInfo (title, description, ISBN) VALUES(
 
 CREATE TABLE bookbasket.threadMetaInfo(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    userID INT,
+    userID VARCHAR(50),
     title VARCHAR(50),
-    ISBN INT 
+    ISBN INT
 );
 
 INSERT INTO bookbasket.threadMetaInfo (userID, title, ISBN) VALUES(
-    '1',
+    'Alice',
     "I don't understand p.32 at all.",
     '100'
 ),
 (
-    '2',
+    'Bob',
     "there is an awful typo on p.55",
     '100'
 );
@@ -39,46 +39,41 @@ INSERT INTO bookbasket.threadMetaInfo (userID, title, ISBN) VALUES(
 
 CREATE TABLE bookbasket.threadMessage(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    userID INT,
+    userID VARCHAR(50),
     message TEXT,
-    threadID INT 
+    threadID INT
 );
 
 INSERT INTO bookbasket.threadMessage (userID, message, threadID) VALUES(
-    '11',
+    'Carol',
     'Me neither.',
     '1'
 ),
 (
-    '12',
+    'Charlie',
     'I think the author tries to say ...',
     '1'
 );
 
 
 CREATE TABLE bookbasket.userInfo(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    userName VARCHAR(50),
+    id VARCHAR(50)PRIMARY KEY,
     password VARCHAR(50)
 );
 
 INSERT INTO bookbasket.userInfo VALUES(
-    '1',
     'Alice',
     'pass'
 ),
 (
-    '2',
     'Bob',
     'word'
 ),
 (
-    '11',
     'Carol',
     'qwer'
 ),
 (
-    '12',
     'Charlie',
     'tyui'
 );
